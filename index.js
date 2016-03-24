c5442429c1917cb58bdf236e287d184c02cb72fa
CJ Bryan <christianbryan@gmail.com>
2016-03-24 14:31:42 -0700

    shots fired!

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+app.get('/', function (request, response) {
+  response.render('index');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
+});
