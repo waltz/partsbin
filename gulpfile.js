165810ea1aeaa414956285e6b02ff669e82c6faa
CJ Bryan <christianbryan@gmail.com>
2016-03-25 16:13:58 -0700

    Get Gulp and LiveReload working together.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
-var gulp = require('gulp');
-var server = require('gulp-express');
+var gulp    = require('gulp'),
+    gls = require('gulp-live-server'),
+    webpack = require('gulp-webpack');
 
 gulp.task('default', function() {
-  server.run(['index.js']);
+  var server = gls.new('index.js');
+  server.start();
 
-  gulp.watch(['./**/*.js'], server.run);
+  var client_scripts = './public/js/entry.js';
+
+  gulp.watch('index.js', server.start.bind(server));
   gulp.watch(['./**/*.css'], function(file) {
-    console.log(file);
-    server.notify(file);
+    console.log('fresh css changes detected');
+    server.notify.apply(server, [file]);
   });
+  gulp.watch('./public/bundle.js', function(file) {
+    console.log('fresh js bundle!');
+    server.notify.apply(server, [file]);
+  })
+
+  gulp.src(client_scripts)
+    .pipe(webpack({
+      watch: true,
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('public/'));
 });
