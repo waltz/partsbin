6c9f4818ca8af41156721c34ad3156a105ccffe4
CJ Bryan <christianbryan@gmail.com>
2016-09-05 14:51:52 -0700

    Add in a few logging messages in the Gulpfile so we know what's happening.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -15,10 +15,12 @@ gulp.task('default', () => {
   gulp.watch('index.js', server.start.bind(server));
 
   gulp.watch('public/index.css', (file) => {
+    console.log('LiveReload found new styles.');
     server.notify.apply(server, [file]);
   });
 
   gulp.watch('public/index.js', (file) => {
+    console.log('LiveReload found new client JavaScript.');
     server.notify.apply(server, [file]);
   });
 
@@ -28,18 +30,20 @@ gulp.task('default', () => {
 });
 
 gulp.task('webpack', () => {
-  gulp.src('./client/js/index.jsx')
+  console.log('Webpacking.');
+
+  return gulp.src('./client/js/index.jsx')
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('public/'));
 });
 
 gulp.task('test', () => {
+  console.log('Running tests.');
+
   return gulp.src('spec/**/*.js').pipe(jasmine());
 });
 
-gulp.task('sass', (things) => {
-  console.log(things());
-
+gulp.task('sass', () => {
   const bourbonPaths = require('bourbon').includePaths;
   const neatPaths = require('bourbon-neat').includePaths;
   const cssPaths = bourbonPaths.concat(neatPaths);
